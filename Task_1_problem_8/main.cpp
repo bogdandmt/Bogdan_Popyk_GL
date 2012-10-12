ea4d05b00a895f38513b9f7031c375955c36cbe7
bogdandmt <bogdandmt@gmail.com>
2012-10-13 01:41:05 +0300

    Update Task_1_problem_8/main.cpp

--- Task_1_problem_8/main.cpp
+++ Task_1_problem_8/main.cpp
@@ -1,12 +1,30 @@
 #include "func_declar.h"
 
-void main()
+void main (void)
 {
-	unsigned long int input, res;
-	cout << "Input unsigned integer\n";
-	cin >> input;
-	cout << "Binary representation of the entered numeric:\n" << DecToBin(input);
-	res = SetFirstBit(input);
+	cout << "Enter unsigned integer\n";
+	//Checking of input format
+	string str;
+	getline (cin, str);
+	if (str.empty())
+	{
+		cout << "Incorrect input format\n";
+		return;
+	}
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if ( !isdigit(str[i]) )
+		{
+			cout << "Incorrect input format\n";
+			return;
+		}
+	}
+
+	ULI input, res;
+	input = atol (str.c_str());
+
+	cout << "Binary representation of the entered numeric:\n" << DecToBin (input) ;
+	res = SetFirstBit (input);
 	cout << "\nDecimal representation of result:\n" << res;
-	cout << "\nBinary representation of the result:\n" << DecToBin(res) << endl;
+	cout << "\nBinary representation of the result:\n" << DecToBin (res) << endl;
 }
