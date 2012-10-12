a11f04b34ce8e4b30ede9194b74a4234c4815aac
bogdandmt <bogdandmt@gmail.com>
2012-10-13 01:38:31 +0300

    Update Task_1_problem_8/func_declar.h

--- Task_1_problem_8/func_declar.h
+++ Task_1_problem_8/func_declar.h
@@ -3,9 +3,11 @@
 
 using namespace std;
 
+typedef unsigned long int ULI;
+
 // Function that converts a decimal representation of the number in binary
-string DecToBin(const unsigned long int &decNumeric);
+string DecToBin (const ULI &decNumeric);
 
 // Problem N8.  Function that returns an unsigned long integer, which is set to 1 only one bit - in the position
-//				of the oldest cleared (set to 0) bits set unsigned long integer (function parameter).
-unsigned long int SetFirstBit(const unsigned long int &unsgnInt);
+//				      of the oldest cleared (set to 0) bits set unsigned long integer (function parameter).
+ULI SetFirstBit (const ULI &unsgnInt) ;
