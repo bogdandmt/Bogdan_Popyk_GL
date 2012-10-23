ae76c10a7d9500b4ba071f95860ddf395456914a
bogdandmt <bogdandmt@gmail.com>
2012-10-23 10:03:46 +0300

    Update Task 3/Exercise 8/lib.h

--- Task 3/Exercise 8/lib.h
+++ Task 3/Exercise 8/lib.h
@@ -1,8 +1,15 @@
 /**
- * @file	lib.h
+ * @file  lib.h
  * @brief	Prototypes of functions for task 3 exercise 8
  */
 
 typedef unsigned char Byte;
 
-void SwapVectorsElements(int *vector1, int *vector2, Byte number);
+/**
+ * @brief	Function swaps elements by number of two vectors of integers
+ * @param	[in, out] vector1 The first vector
+ * @param	[in, out] vector2 The second vector
+ * @param	[in] number Number of swapping elements
+ * @return	void
+ */
+void SwapVectorsElements(int *vector1, int *vector2, const Byte &number);
