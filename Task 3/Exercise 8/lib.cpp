5ad0ced167542dcd8945df6cffdc401d59024b2f
bogdandmt <bogdandmt@gmail.com>
2012-11-01 14:58:37 +0200

    Update Task 3/Exercise 8/lib.cpp

--- Task 3/Exercise 8/lib.cpp
+++ Task 3/Exercise 8/lib.cpp
@@ -5,9 +5,64 @@
 
 #include "lib.h"
 
-void SwapVectorsElem(int *vec1, int *vec2, Byte numb)
+void SwapVecElem(int *vec1, int *vec2, Byte numb)
 {
-	int temp = *(vec1 + numb);
-	*(vec1 + numb) = *(vec2 + numb);
-	*(vec2 + numb) = temp;
+	if(vec1 && vec2)
+	{
+		int temp = *(vec1 + numb);
+		*(vec1 + numb) = *(vec2 + numb);
+		*(vec2 + numb) = temp;
+	}
+	else
+	{
+		printf("Vector is not valid");
+	}
+}
+
+Byte EnterRowsColsNumb(char *chPtr, const char *msg)
+{
+	if (chPtr)
+	{
+		bool exit = false;
+		int iTemp;
+		Byte linesNumb;
+		do
+		{
+			printf("\tEnter number of %s <= %u :\n\t", msg, ROWS_COLS_MAX);
+			scanf("%4s", chPtr);
+			printf("\n");
+			iTemp = atoi(chPtr);
+			if (iTemp <= ROWS_COLS_MAX && iTemp >= ROWS_COLS_MIN && *chPtr != '0')
+			{
+				linesNumb = iTemp;
+				exit = true;
+			}
+			else
+			{
+				printf("\n\tIncorrect format of input data. Try again.\n");
+			}		
+			fflush(stdin);
+		}
+		while (!exit);
+		return linesNumb;
+	}
+	else
+	{
+		printf("Invalid pointer to char\n");
+		return 0U;
+	}
+}
+
+void PrintMatrix(const int *matrix, Byte rowsNumb, Byte colsNumb)
+{
+	Byte i, j;
+	for (i = 0; i < rowsNumb; ++i)
+	{
+		printf("\t");
+		for (j = 0; j < colsNumb; ++j)
+		{
+			printf("%d\t", *(matrix + colsNumb * i + j) );
+		}
+		printf("\n");
+	}
 }
