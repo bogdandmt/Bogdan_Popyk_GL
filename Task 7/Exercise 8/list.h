da7c2221bfdb9bab3643feb7cbecdc611f6f061b
Bogdan Popyk <bogdandmt@gmail.com>
2012-12-09 23:41:50 +0200

    Edited and added new functions
    Edited: void List<T>::PushBack(const T &)
    Added: List<T>& List<T>::operator=(const List<T> &)

--- Task 7/Exercise 8/list.h
+++ Task 7/Exercise 8/list.h
@@ -8,82 +8,97 @@
 
 typedef unsigned short int USI;
 
-template <class T>
+template <class T> 
 struct Item
 {
 	T data;
 	Item<T> *next;
+
 	Item() : data(), next(0) {}
-	Item(const T &_data) : data(_data), next(0) {}
+
+	Item(const Item<T> &i) : data(i.data), next(i.next) {}
+
+	Item(const T &_data) : data(_data), next(0) {}	
 };
 
-template <class T>
+template <class T> 
 class List
 {
 	Item<T> *begin, *end;
 	USI size;
 public:
-	List();
+	List() : begin(0), end(0), size(0) {}
 
 	List(const List<T> &l);
 
+	List<T>& operator=(const List<T> &l);
+
 	USI Size() const;
 
-	void Print() const;
+	void Print(std::ostream &out) const;
 
 	void PushBack(const T &elem);
 
 	void Union(List<T> &l);
 };
 
-template <class T>
-List<T>::List() : begin(0), end(0), size(0) {}
-
-template <class T>
-List<T>::List(const List<T> &l)
+template <class T> 
+List<T>::List(const List<T> &l) : begin(0), end(0), size(0)
 {
 	if(this != &l && l.Size() && l.begin)
 	{
-		//Item<T> *i = l.begin;
-		////begin = l.begin;
-		//Item<T> *j = begin;
-		//Item<T> *next;
-		//while (i)
-		//{
-		//	Item<T> tmp(i -> data);
-		//	next = &tmp;
-		//	j = &tmp;
-		//	next = j -> next;
-		//}
+		Item<T> *i = l.begin;
+		begin = 0;
+		while (i)
+		{
+			this->PushBack(i->data);
+			i = i -> next;
+		}
 	}
 }
 
-template <class T>
+template <class T> 
+List<T>& List<T>::operator=(const List<T> &l)
+{
+	if(this != &l && l.Size() && l.begin)
+	{
+		Item<T> *i = l.begin;
+		begin = 0;
+		while (i)
+		{
+			this->PushBack(i->data);
+			i = i -> next;
+		}
+	}
+	return *this;
+}
+
+template <class T> 
 USI List<T>::Size() const
 {
 	return size;
 }
 
-template <class T>
-void List<T>::Print() const
+template <class T> 
+void List<T>::Print(std::ostream &out) const
 {
 	Item<T> *i = begin;
 	while (i)
 	{
-		cout << i -> data <<" ";
+		out << i -> data <<" ";
 		i = i -> next;
 	}
 }
 
-template <class T>
+template <class T> 
 void List<T>::PushBack(const T &elem)
 {
-	Item<T> *tmp = new Item<T>(elem);
-	if (tmp)
+	if (begin)
 	{
-		if (begin)
+		if (end)
 		{
-			if (end)
+			Item<T> *tmp = new Item<T>(elem);
+			if (tmp)
 			{
 				end -> next = tmp;
 				end = tmp;
@@ -91,24 +106,39 @@ void List<T>::PushBack(const T &elem)
 			}
 			else
 			{
-				cout << "NULL pointer\n";
+				cout << "Memory was not allocated\n";
 			}
 		}
 		else
 		{
-			begin = end = tmp;
-			++size;
+			cout << "NULL pointer\n";
 		}
 	}
 	else
 	{
-		cout << "Memory was not allocated\n";
-	}
+		Item<T> *tmp = new Item<T>(elem);
+		if (tmp)
+		{
+			begin = end = tmp;
+			++size;
+		}
+		else
+		{
+			cout << "Memory was not allocated\n";
+		}
+	}	
 }
 
-template <class T>
+template <class T> 
 void Union(List<T> &l)
 {
+}
+
+template <class T> 
+std::ostream& operator << (std::ostream &out, const List<T> &l)
+{
+	l.Print(out);
+	return out;
 }
 
 #endif /* _LIST_H_ */
--- Task 7/Exercise 8/main.cpp
+++ Task 7/Exercise 8/main.cpp
@@ -4,14 +4,32 @@
  */
 
 #include "incl.h"
+#include <list>
 
 int main(void)
 {
 	List<int> l;
 	l.PushBack(1);
 	l.PushBack(2);
-	l.Print();
+	l.Print(cout);
 	List<int> l2(l);
-	//l2.Print();
+	l.PushBack(3);
+
+	cout<<endl;
+	l2.Print(cout);
+	cout<<endl;
+	l.Print(cout);
+	cout<<endl<<l2.Size()<<"\n\n<<\n";
+	cout<<l;
+
+	//Item<int> i(4);
+	//const Item<int> &r= 5;
+	//Item<int> j(r);
+	//cout<<j.data;
+	//const Item<int> z(6);
+	//Item<int> i=z;
+
+	list<int> stl;
+	//stl.pu
 	return 0;
 }
